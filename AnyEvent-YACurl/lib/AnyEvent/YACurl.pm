72ae1c7c4c86301472e38460e780723cccd91e6d
Tom van der Woerdt <tom.vanderwoerdt@booking.com>
2019-04-08 18:05:42 +0200

    0.07: include curl 7.64.1

--- AnyEvent-YACurl/lib/AnyEvent/YACurl.pm
+++ AnyEvent-YACurl/lib/AnyEvent/YACurl.pm
@@ -6,10 +6,10 @@ use warnings;
 
 use AnyEvent;
 
-our $VERSION = '0.06_001';
+our $VERSION = '0.07';
 
 require XSLoader;
-XSLoader::load('AnyEvent::YACurl', '0.06');
+XSLoader::load('AnyEvent::YACurl', '0.07');
 
 require constant;
 my %constants= %{_get_known_constants()};
