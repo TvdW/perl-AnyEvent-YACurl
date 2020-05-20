8cce808604126b98e1d9b817f29a31d40c795ac9
Tom van der Woerdt <tom.vanderwoerdt@booking.com>
2020-05-20 16:29:24 +0200

    0.15

--- AnyEvent-YACurl/lib/AnyEvent/YACurl.pm
+++ AnyEvent-YACurl/lib/AnyEvent/YACurl.pm
@@ -6,10 +6,10 @@ use warnings;
 
 use AnyEvent;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 require XSLoader;
-XSLoader::load('AnyEvent::YACurl', '0.14');
+XSLoader::load('AnyEvent::YACurl', '0.15');
 
 require constant;
 my %constants= %{_get_known_constants()};
