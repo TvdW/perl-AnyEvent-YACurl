3aad908050a053f1a2ca55acfc31b79e90a397f1
Tom van der Woerdt <info@tvdw.eu>
2020-10-14 16:14:52 +0200

    Remove libbrotli
    Trying to make this package a bit leaner, and libbrotli would go great
    as an external dependency the user can choose to opt in to.

--- AnyEvent-YACurl/build.sh
+++ AnyEvent-YACurl/build.sh
@@ -3,12 +3,10 @@ set -eo pipefail
 CURLVER=7.73.0
 CARESVER=1.16.1
 NGHTTP2VER=1.41.0
-BROTLIVER=0.1.0
 
 rm -rf nghttp2-$NGHTTP2VER nghttp2-lib nghttp2-src
 rm -rf curl-$CURLVER curl-src curl-lib
 rm -rf c-ares-$CARESVER c-ares-src c-ares-lib
-rm -rf libbrotli-$BROTLIVER libbrotli-src libbrotli-lib
 
 tar -xf nghttp2-$NGHTTP2VER.tar.gz
 mv nghttp2-$NGHTTP2VER nghttp2-src
@@ -18,9 +16,6 @@ mv curl-$CURLVER curl-src
 
 tar -xf c-ares-$CARESVER.tar.gz
 mv c-ares-$CARESVER c-ares-src
-
-tar -xf libbrotli-$BROTLIVER.tar.gz
-mv libbrotli-$BROTLIVER libbrotli-src
 
 for file in patch/*; do
     patch -p1 < $file
@@ -42,4 +37,3 @@ cp MANIFEST.pre MANIFEST
 find curl-src -type f >> MANIFEST
 find nghttp2-src -type f >> MANIFEST
 find c-ares-src -type f >> MANIFEST
-find libbrotli-src -type f >> MANIFEST
