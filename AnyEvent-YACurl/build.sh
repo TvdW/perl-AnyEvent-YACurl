34acba67d87633dce62c5348023305d567f2bd55
Tom van der Woerdt <info@tvdw.eu>
2021-03-15 16:34:25 +0100

    0.21

--- AnyEvent-YACurl/build.sh
+++ AnyEvent-YACurl/build.sh
@@ -1,6 +1,6 @@
 set -e
 
-CURLVER=7.74.0
+CURLVER=7.75.0
 [ -f curl-$CURLVER.tar.gz ] || curl --output curl-$CURLVER.tar.gz https://curl.se/download/curl-$CURLVER.tar.gz
 tar -xf curl-$CURLVER.tar.gz
 mv curl-$CURLVER curl-src
