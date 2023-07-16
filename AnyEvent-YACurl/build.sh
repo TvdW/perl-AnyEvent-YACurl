6f7653ad0e8c9b3289c2867663c634925cff9a44
Tom van der Woerdt <info@tvdw.eu>
2023-07-16 08:01:30 +0700

    Generate curlopt info based on the local curl version
    This gives a significantly greater compatibility range with older and
    newer curl versions. The amount of constants is now also significantly
    increased, having added deprecated ones as well.

--- AnyEvent-YACurl/build.sh
+++ AnyEvent-YACurl/build.sh
@@ -1,6 +1,6 @@
 set -e
 
-CURLVER=7.75.0
+CURLVER=8.1.2
 [ -f curl-$CURLVER.tar.gz ] || curl --output curl-$CURLVER.tar.gz https://curl.se/download/curl-$CURLVER.tar.gz
 tar -xf curl-$CURLVER.tar.gz
 mv curl-$CURLVER curl-src
@@ -9,5 +9,5 @@ cd curl-src/docs/libcurl
 perl symbols.pl > ../../../libcurl-symbols.h
 cd ../../..
 
-perl sync-symbols.pl
+perl sync-constants.pl
 rm -rf curl-src
